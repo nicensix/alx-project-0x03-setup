a25091c91ba20475f7dd03ddc5433542b35ccdc5
Emmanuel Godwin <nicensix@gmail.com>
2025-06-15 21:13:14 +0300

    Initial commit

---
+++ alx-project-0x03/components/layouts/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="bg-gray-800 text-white py-4 text-center mt-auto">
+      &copy; {new Date().getFullYear()} ALX Project – All rights reserved.
+    </footer>
+  );
+};
+
+export default Footer;
